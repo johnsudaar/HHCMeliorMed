66c62b6a1897783912dd996314c3b14f906889f7
Anthony Martin <martin.anthonyq@gmail.com>
2015-03-21 14:28:48 +0100

    Modification BDD + Ajout de fonction upload

--- BDD/meliormed.sql
+++ BDD/meliormed.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Sam 21 Mars 2015 à 01:30
+-- Généré le: Sam 21 Mars 2015 à 14:21
 -- Version du serveur: 5.5.41-0ubuntu0.14.04.1
 -- Version de PHP: 5.5.9-1ubuntu4.7
 
@@ -19,6 +19,20 @@ SET time_zone = "+00:00";
 --
 -- Base de données: `meliormed`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `chatmessage`
+--
+
+CREATE TABLE IF NOT EXISTS `chatmessage` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `sender` int(10) NOT NULL,
+  `dest` int(10) NOT NULL,
+  `message` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -58,6 +72,28 @@ CREATE TABLE IF NOT EXISTS `fileRequest` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `notif`
+--
+
+CREATE TABLE IF NOT EXISTS `notif` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `dest` int(10) NOT NULL,
+  `reply_id` int(10) NOT NULL,
+  `request_id` int(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `notif`
+--
+
+INSERT INTO `notif` (`id`, `dest`, `reply_id`, `request_id`) VALUES
+(1, 1, 0, 1),
+(2, 2, 0, 1);
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `reply`
 --
 
@@ -68,7 +104,16 @@ CREATE TABLE IF NOT EXISTS `reply` (
   `resolu` int(11) NOT NULL,
   `idUser` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `reply`
+--
+
+INSERT INTO `reply` (`id`, `message`, `request`, `resolu`, `idUser`) VALUES
+(1, 'Ok thx', 1, 0, 2),
+(2, '2riz1', 1, 1, 3),
+(3, 'Lol g pa lu', 2, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -83,7 +128,16 @@ CREATE TABLE IF NOT EXISTS `request` (
   `message` text NOT NULL,
   `type` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `request`
+--
+
+INSERT INTO `request` (`id`, `idUser`, `titre`, `message`, `type`) VALUES
+(1, 1, 'Test', 'Je suis la pour le test', 'pub'),
+(2, 2, 'Test2', 'Je suis aussi la pour le test', 'ann'),
+(3, 3, 'Test3', 'Ah toi aussi ? Moi aussi !', 'requ');
 
 -- --------------------------------------------------------
 
@@ -107,7 +161,19 @@ CREATE TABLE IF NOT EXISTS `tag` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `libelle` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+
+--
+-- Contenu de la table `tag`
+--
+
+INSERT INTO `tag` (`id`, `libelle`) VALUES
+(1, 'Clandestin'),
+(2, 'Cardiologie'),
+(3, 'Métrologie'),
+(4, 'Soleillogie'),
+(5, 'Boudoulogie'),
+(6, 'Mongolie');
 
 -- --------------------------------------------------------
 
@@ -125,17 +191,17 @@ CREATE TABLE IF NOT EXISTS `user` (
   `ville` text NOT NULL,
   `adresse` text NOT NULL,
   `photo` text NOT NULL,
-  `tags` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
-CREATE TABLE IF NOT EXISTS `notif` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `dest` int(10) NOT NULL,
-  `reply_id` int(10) NOT NULL,
-  `request_id` int(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+--
+-- Contenu de la table `user`
+--
+
+INSERT INTO `user` (`id`, `nom`, `prenom`, `fonction`, `pays`, `etablissement`, `ville`, `adresse`, `photo`) VALUES
+(1, 'HURTER', 'Jonathan', 'Dev', 'France', 'ENSIIE', 'Illkirch', '9 rue des bananes', 'profiles/1.jpeg'),
+(2, 'KOBERSI', 'Pauline', 'Dev', 'France', 'ENSIIE', 'Illkirch', '9 rue des bananes', 'profiles/2.jpeg'),
+(3, 'MARTIN', 'Anthony', 'Dev', 'France', 'CNAM', 'Strasbourg', '10 rue des patates', 'profiles/3.jpeg');
 
 -- --------------------------------------------------------
 
@@ -149,13 +215,17 @@ CREATE TABLE IF NOT EXISTS `userTag` (
   PRIMARY KEY (`idUser`,`idTag`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `chatmessage` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `sender` int(10) NOT NULL,
-  `dest` int(10) NOT NULL,
-  `message` text NOT NULL,
-  PRIMARY KEY(`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT = 1;
+--
+-- Contenu de la table `userTag`
+--
+
+INSERT INTO `userTag` (`idUser`, `idTag`) VALUES
+(1, 1),
+(1, 2),
+(2, 3),
+(2, 4),
+(3, 4),
+(3, 5);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- functions.php
+++ functions.php
@@ -45,4 +45,35 @@ function logout(){
 	unset($_SESSION['login']);
 }
 
+/*function handleFileUpload($files){
+	$target_dir = "uploads/";
+	$target_file = $target_dir . $files["fileToUpload"]["name"];
+	$uploadOk = 1;
+	// Check if image file is a actual image or fake image
+	if(isset($_POST["submit"])) {
+		$uploadOk = 1;
+	}
+	// Check if file already exists
+	if (file_exists($target_file)) {
+    	echo "Sorry, file already exists.";
+    	$uploadOk = 0;
+	}
+	 // Check file size
+	if ($files["fileToUpload"]["size"] > 20000000) {
+    	echo "Sorry, your file is too large.";
+    	$uploadOk = 0;
+    }
+    // Check if $uploadOk is set to 0 by an error
+	if ($uploadOk == 0) {
+	    echo "Sorry, your file was not uploaded.";
+	// if everything is ok, try to upload file
+	} else {
+	    if (move_uploaded_file($files["fileToUpload"]["tmp_name"], $target_file)) {
+	        echo "The file ". basename( $files["fileToUpload"]["name"]). " has been uploaded.";
+	    } else {
+	        echo "Sorry, there was an error uploading your file.";
+	    }
+	}
+}*/
+
 ?>
